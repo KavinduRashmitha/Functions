764b414fac86016a800f7a47fc2c199e231d3702
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-12 23:33:45 +0530

    version-2

--- fibanacci.py
+++ fibanacci.py
@@ -6,7 +6,21 @@ def WhichFib(n):
         i,j=j,i+j
     return i
 
+def WhichFib_While(n):
+    i=0
+    j=1
+    count=0
+    while count<n:
+        count+=1
+        i,j=j,i+j
+    return i
+
 num=int(input("Which Fibanacci Number Do You Want: "))
 x="Fib({}) = {}"
 x=x.format(num,WhichFib(num))
+print(x)
+
+num=int(input("Which Fibanacci Number Do You Want: "))
+x="Fib({}) = {}"
+x=x.format(num,WhichFib_While(num))
 print(x)
