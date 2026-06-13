47d2cb6eeb9e4c63e78657e87ccff399e6082d62
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-13 07:37:43 +0530

    version-2

--- sorted_or_not.py
+++ sorted_or_not.py
@@ -1,4 +1,5 @@
 "***This is a Function to Check Whether the Given list is Sorted or Not***"
+#ForLoop
 def IsItSorted(l):
     is_sorted=True
     next=l[0]
@@ -10,6 +11,24 @@ def IsItSorted(l):
             next=i
     return is_sorted
 
+
+#WhileLoop
+def IsItSortedWhile(l):
+    is_sorted=True
+    before=l[0]
+    count=1
+    while count<len(l):
+        if l[count]<before:
+            is_sorted=False
+            break
+        before=l[count]
+        count+=1
+    return is_sorted
+
 nums=input("Enter values: ").split(",")
 nums=list(map(int,nums))
 print(IsItSorted(nums))
+
+nums=input("Enter values: ").split(",")
+nums=list(map(int,nums))
+print(IsItSortedWhile(nums))
