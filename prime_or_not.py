9c5efd21a944e1e32b65040359ab0663836e05d2
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-13 07:14:16 +0530

    version-2

--- prime_or_not.py
+++ prime_or_not.py
@@ -7,5 +7,18 @@ def IsPrime(n):
             break
     return is_prime
 
+def IsPrimeWhile(n):
+    is_prime=True
+    count=2
+    while count<n:
+        if n%count==0:
+            is_prime=False
+            break
+        count+=1
+    return is_prime
+
 num=int(input("Enter the Value: "))
 print("Is",num,"Prime :",IsPrime(num))
+
+num=int(input("Enter the Value: "))
+print("Is",num,"Prime :",IsPrimeWhile(num))
